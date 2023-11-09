9052ca1fa866d3bafcbd73fd301ec6c2dae87e33
Morii <lawrence.han.shh@outlook.com>
2023-11-09 18:11:49 +0800

    修复 在没有连接到服务器的情况下，点击发送就闪退的bug

--- app/src/main/java/com/example/chatclient/MainActivity.java
+++ app/src/main/java/com/example/chatclient/MainActivity.java
@@ -1,33 +1,23 @@
 package com.example.chatclient;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
-import java.io.DataInputStream;
+//import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    private int BUFFER_SIZE = 1024;
-
+    private final int BUFFER_SIZE = 1024;
     private TextView showmsg;
     private EditText sendmsged;
     private Button sendmsgbt;
@@ -37,16 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private DataOutputStream out;
     private Socket socket;
     private String receiveTxt,SendMsg="";//接收文本 发送文本
-    private static String[] PERMISSIONS_STORAGE = {
-            //依次权限申请
-            Manifest.permission.INTERNET
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        applypermission();//权限申请
         InitView();
         connectionSeverThread.start();
         SendMsgThread.start();
@@ -58,9 +42,6 @@ public class MainActivity extends AppCompatActivity {
                     showmsg.append("我："+sendmsged.getText().toString()+"\n");
                     sendmsged.setText("");
                 }else if(msg.what==98){
-//                    showmsg.append("您有一条新消息");
-//                    String strdata = (String)msg.obj;
-//                    showmsg.append(strdata);
                     showmsg.append(receiveTxt);
                 }
             }
@@ -75,16 +56,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private void InitView() {
-        showmsg = (TextView) findViewById(R.id.showmsg);
-        sendmsged = (EditText) findViewById(R.id.sendmsged);
-        sendmsgbt = (Button) findViewById(R.id.sendmsgbt);
-
-        //showmsg.setMovementMethod(ScrollingMovementMethod.getInstance());//textview滚动 但控件会随键盘浮动
-//        用此功能xml的textview控件添加如下两段
-//        android:scrollbars="vertical"
-//        android:fadeScrollbars="false"
+        showmsg = findViewById(R.id.showmsg);
+        sendmsged = findViewById(R.id.sendmsged);
+        sendmsgbt = findViewById(R.id.sendmsgbt);
     }
-
 
     Thread connectionSeverThread = new Thread(
             //连接服务器
@@ -92,14 +67,13 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     try {
-                        //获取编辑框组件
+                        //新建socket对象
                         socket = new Socket("192.168.1.104", 8000);
                         //连接服务器
                         out= new DataOutputStream(socket.getOutputStream());
                         // 创建DataOutputStream对象 发送数据
                         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     } catch (Exception e) {
-                        // TODO Auto-generatetd catch block
                         e.printStackTrace();
                     }
                     try {
@@ -124,9 +98,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     while (true){
-                        if(SendMsg != ""){
+                        if(SendMsg != "" && out!=null){
                             try {
-                                out.writeUTF(SendMsg);//发送消息
+                                    out.writeUTF(SendMsg);//发送消息
+
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -138,25 +113,7 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                     }
-
                 }
             }
     );
-
-    //定义判断权限申请的函数，在onCreat中调用就行
-    public void applypermission(){
-        if(Build.VERSION.SDK_INT>=23){
-            boolean needapply=false;
-            for(int i=0;i<PERMISSIONS_STORAGE.length;i++){
-                int chechpermission= ContextCompat.checkSelfPermission(getApplicationContext(),
-                        PERMISSIONS_STORAGE[i]);
-                if(chechpermission!= PackageManager.PERMISSION_GRANTED){
-                    needapply=true;
-                }
-            }
-            if(needapply){
-                ActivityCompat.requestPermissions(MainActivity.this,PERMISSIONS_STORAGE,1);
-            }
-        }
-    }
 }
